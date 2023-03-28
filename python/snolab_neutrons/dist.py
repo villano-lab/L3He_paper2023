e85e4484e944fa2b75e044041053f4be18b88326
Anthony Villano <anthony.villano@ucdenver.edu>
2023-03-28 14:58:33 -0600

    [ANV] updated library

--- python/snolab_neutrons/dist.py
+++ python/snolab_neutrons/dist.py
@@ -6,6 +6,11 @@ import itertools
 import pickle
 from scipy import signal
 
+#matplotlib for plotting
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+plt.style.use('../mplstyles/stylelib/standard.mplstyle')
+
 # constants
 snothermal = 4144.9/10000/86400
 
@@ -38,6 +43,27 @@ def smooth_snolab_LEflux(cutoff=0.3,file='data/FDF.txt'):
     #smooth the data
     ff_smooth = signal.savgol_filter(ff_trim, 75, 3) # window size 75, polynomial order 3
     return Eff_trim,ff_smooth
+
+def extrapolate_snolab_flux(Ef,f,low=0.5e-6,high=2e-2,plot=False):
+
+    #fast_lin_df = ffle[ffle < E_thresh]
+    #fast_lin_df_spec = fflespec_smooth[ffle< E_thresh]
+    Ef_trim = Ef[Ef<high]
+    f_trim = f[Ef<high]
+    fitted_line = ss.linregress(np.log(Ef_trim), np.log(f_trim))
+    # return height of fitted loglog line, if energy is larger than thermal threshold
+
+    func = lambda E: np.exp(fitted_line.intercept + fitted_line.slope*np.log(E))*(E > low)
+
+    #plot if requested
+    if plot:
+       fig = plt.figure()
+       ax = fig.add_subplot(111)
+       ax.set_title('fit w/ points')
+       ax.plot(Ef_trim, f_trim, 'o')
+       ax.plot(Ef_trim, func(Ef_trim), linestyle = 'dashed')
+
+    return func 
 
 def read_snolab_HEflux(file='data/FDF.txt'):
     # read in fast neutron flux spectrum (from reading_n_spectra.ipynb)
