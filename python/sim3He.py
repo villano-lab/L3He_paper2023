7b06cec390ed50810a7b9bea9a02948a361102f2
Anthony Villano <anthony.villano@ucdenver.edu>
2023-07-13 10:17:55 -0400

    [ANV] updated sim3He library

--- python/sim3He.py
+++ python/sim3He.py
@@ -4,6 +4,13 @@ import scipy.interpolate as inter
 import scipy.constants as co
 import h5py
 
+#get library path
+import os.path
+import sys
+current_module = sys.modules[__name__]
+
+mod_path = os.path.split(current_module.__file__)[0]
+
 #a crude function to get liquid or gas density pressure (p) = None means liquid
 def get3HeDensity(p=None):
 
@@ -30,3 +37,22 @@ def get3HeDensity(p=None):
   rhogas = rhogas_mol*masshe3
   
   return [p*rhogas,p*ngas*co.N_A] 
+
+def getSNOLABnFlux():
+  fast_flux_df = pd.read_pickle('{}/data/FDF.txt'.format(mod_path)) # 'E' in MeV, 'spec' in neutrons cm^-2 sec^-1 MeV^-1
+  print(fast_flux_df)
+
+#some useful functions
+def integrate_df(df):
+  # (left-sided rectangular integral)
+  dE = -df['E'].diff(periods = -1)
+  dE.iat[-1] = dE.iat[-2]
+  A = df['spec']*dE
+  return A.sum()
+
+def maxwell(E, B):
+  # height of Maxwell distribution for energy E (eV) and temperature T (K) related to B via B = 1/kT
+  return 2*B*np.sqrt(B*E/np.pi)*np.exp(-B*E)
+
+
+
